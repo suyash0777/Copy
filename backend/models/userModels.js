c7dbc850a38d6341b02d5ab88d6163cbdc94a029
suyash0777 <suyashpatil972@gmail.com>
2023-09-13 17:43:22 +0530

    Implement order placed functionality

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -78,4 +78,4 @@ userSchema.methods.getResetPasswordToken = function () {
   return resetToken;
 };
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
