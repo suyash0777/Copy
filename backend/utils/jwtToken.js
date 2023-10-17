67462e7946d0b2230130dc12637d3e8f59dd9314
suyash0777 <suyashpatil972@gmail.com>
2023-10-17 14:09:35 +0530

    44

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,30 +1,5 @@
-// const sendToken = (user, statusCode, res) => {
-//   const token = user.getJWTToken();
-
-//   // Options for the cookie
-//   const options = {
-//     expires: new Date(
-//       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-//     ),
-//     // httpOnly: true,
-//     secure: true,
-//     sameSite: "None",
-//     domain: ".newshop-b4ff.onrender.com",
-//     path: "/api/v1",
-//   };
-
-//   res.status(statusCode).cookie("token", token, options).json({
-//     success: true,
-//     user,
-//     token,
-//   });
-// };
-
-// module.exports = sendToken;
-
 const sendToken = (user, statusCode, res) => {
-  const token = user.getJWToken();
-  console.log(`Token: ${token}`);
+  const token = user.getJWTToken();
 
   // Options for the cookie
   const options = {
@@ -46,3 +21,28 @@ const sendToken = (user, statusCode, res) => {
 };
 
 module.exports = sendToken;
+
+// const sendToken = (user, statusCode, res) => {
+//   const token = user.getJWToken();
+//   console.log(`Token: ${token}`);
+
+//   // Options for the cookie
+//   const options = {
+//     expires: new Date(
+//       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+//     ),
+//     // httpOnly: true,
+//     secure: true,
+//     sameSite: "None",
+//     domain: ".newshop-b4ff.onrender.com",
+//     path: "/api/v1",
+//   };
+
+//   res.status(statusCode).cookie("token", token, options).json({
+//     success: true,
+//     user,
+//     token,
+//   });
+// };
+
+// module.exports = sendToken;
